b22eff159dd6b57704b8f5d79a08972ff31634fb
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-11-11 06:43:07 -0500

    Initial Astro setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://spencerkarofsky.com',
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: true,
+    },
+  },
+});
