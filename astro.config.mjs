e9edc5384dd4f2245bc3b3d41c1b4c8e31312254
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-11-16 12:28:09 -0500

    Remove Tailwind from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://spencerkarofsky.com',
-  integrations: [tailwind(), sitemap()],
   output: 'static',
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  vite: {
-    build: {
-      cssMinify: true,
-      minify: true,
-    },
-  },
 });
